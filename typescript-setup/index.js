789e4e1638422f7117b5531c30a96261a43f1d38
fedeval <federico.valenza5@gmail.com>
2021-04-19 23:25:05 +0200

    completed: typescript setup exercise

---
+++ typescript-setup/index.js
@@ -0,0 +1,4 @@
+"use strict";
+var myName = "Federico";
+var hello = function (userName) { return "hello, " + userName; };
+console.log(hello(myName));
---
+++ typescript-setup/index.ts
@@ -0,0 +1,5 @@
+const myName = "Federico";
+
+const hello = (userName: string): string => `hello, ${userName}`;
+
+console.log(hello(myName));
