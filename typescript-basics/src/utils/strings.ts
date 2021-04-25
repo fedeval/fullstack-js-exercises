917afcd1a44c6e775c26166621434bf1bd0c267e
fedeval <federico.valenza5@gmail.com>
2021-04-25 18:34:17 +0200

    solving: add explicit typing to string.ts

--- typescript-basics/src/utils/strings.ts
+++ typescript-basics/src/utils/strings.ts
@@ -1,19 +1,19 @@
-const concat = (str1, str2) => {
+const concat = (str1: string, str2: string): string => {
   return str1 + str2;
 };
 
-const capitalize = (str) => {
+const capitalize = (str: string): string => {
   const newStr = str.split(' ')
     .map(word => word[0].toUpperCase() + word.substr(1))
     .join(' ');
   return newStr;
 };
 
-const upperCase = (str) => {
+const upperCase = (str: string): string => {
   return str.toUpperCase();
 };
 
-const lowerCase = (str) => {
+const lowerCase = (str: string): string => {
   return str.toLowerCase();
 };
 
